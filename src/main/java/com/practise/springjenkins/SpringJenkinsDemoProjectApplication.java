e395e7d6824eadaae251961152641a7f07518a7e
kmestry <61279086+kmestry@users.noreply.github.com>
2021-05-16 21:25:45 +0530

    Test project for jenkins pipeline

--- src/main/java/com/practise/springjenkins/SpringJenkinsDemoProjectApplication.java
+++ src/main/java/com/practise/springjenkins/SpringJenkinsDemoProjectApplication.java
@@ -1,13 +1,24 @@
 package com.practise.springjenkins;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+@Slf4j
 public class SpringJenkinsDemoProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringJenkinsDemoProjectApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+    	log.info("Application started successfully!");
+        SpringApplication.run(SpringJenkinsDemoProjectApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("Application is starting.. Inside Init method");
+    }
 
 }
