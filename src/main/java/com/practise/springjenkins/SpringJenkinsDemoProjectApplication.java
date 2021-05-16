7edcae0e50aae8176df4f1179d2dc51dec3be0f0
kmestry <61279086+kmestry@users.noreply.github.com>
2021-05-16 21:13:33 +0530

    Test project for jenkins pipeline

---
+++ src/main/java/com/practise/springjenkins/SpringJenkinsDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.practise.springjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practise/springjenkins/SpringJenkinsDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.springjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
