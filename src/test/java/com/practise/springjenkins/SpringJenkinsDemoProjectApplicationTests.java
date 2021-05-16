da13323e52a2966957f905c7b3dae055e4bf46d4
kmestry <61279086+kmestry@users.noreply.github.com>
2021-05-16 21:57:16 +0530

    Test project for jenkins pipeline

--- src/test/java/com/practise/springjenkins/SpringJenkinsDemoProjectApplicationTests.java
+++ src/test/java/com/practise/springjenkins/SpringJenkinsDemoProjectApplicationTests.java
@@ -1,13 +1,21 @@
 package com.practise.springjenkins;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@Slf4j
 @SpringBootTest
 class SpringJenkinsDemoProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        log.info("Test case executing...");
+		Assertions.assertTrue(true);
+    }
 
 }
